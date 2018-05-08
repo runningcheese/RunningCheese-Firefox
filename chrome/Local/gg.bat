3c5f50af0d397171fb6899d3289ae378292e3fd5
RunningChese <406417036@qq.com>
2018-05-09 00:06:54 +0800

    update

--- chrome/Local/gg.bat
+++
@@ -1,9 +0,0 @@
-@echo off 
-cd /d %~dp0
-::更新视频播放器
-set dd=wget.exe --no-check-certificate  -N -P ..\SubScript\
- 
-::youku
-%dd% https://raw.githubusercontent.com/runningcheese/RunningCheese-Firefox/master/chrome/SubScript/QuickSnapshot.uc.js
-
-
