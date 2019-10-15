b76c2b555c559e08648d9536889ff2ea91e4e2c2
RunningChese <406417036@qq.com>
2019-10-15 14:55:53 +0800

    update

---
+++ Updates/UpdatesFiles/Tabplus.uc.js
@@ -0,0 +1,175 @@
+// ==UserScript==
+// @name           Tabplus.uc.js
+// @description    设置标签的打开方式
+// @update         2019-09-23
+// @update         2019-09-14
+// @update         2018-04-20
+// @license          MIT License
+// @compatibility  Firefox 70+
+// @charset         UTF-8
+// @include       chrome://browser/content/browser.xhtml
+// @include      chrome://browser/content/browser.xul
+// ==/UserScript==
+
+
+// 01. 自动切换到鼠标移动到的标签页 
+/*((g, w) => {
+  class TabPlus {
+    constructor() {
+      this.SelectedTabOnMouseover();
+    }
+    SelectedTabOnMouseover(timeout) {
+      g.tabContainer.addEventListener('mouseover', e => {
+        const tab = e.target.closest('.tabbrowser-tab');
+        if (!tab) return;
+        timeout = setTimeout(() => g.selectedTab = tab, 150);
+      }, false);
+      g.tabContainer.addEventListener('mouseout', () => clearTimeout(timeout), false);
+    }
+  }
+  new TabPlus();
+})(gBrowser, window);
+*/
+
+// 02. 右键关闭标签页
+/*
+gBrowser.tabContainer.addEventListener("click",
+function(event) {
+    if (event.button == 2 && !event.ctrlKey) {
+        const tab = event.target.closest('.tabbrowser-tab');
+        if(!tab) return;
+        gBrowser.removeTab(tab);
+        gBrowser.removeTab(tab, {animate: false});
+        event.stopPropagation();
+        event.preventDefault();
+    }
+},
+false);
+*/
+
+
+
+// 03. 双击标签页关闭标签页
+gBrowser.tabContainer.addEventListener('dblclick', function(event) {
+  if (event.target.localName == 'tab' && event.button == 0) {
+    gBrowser.removeTab(event.target, {animate: false});
+  }
+});
+
+
+
+
+// 04. 标签栏鼠标滚轮切换标签页 
+(function() {
+  if (!location.href.startsWith('chrome://browser/content/browser.x'))
+    return;
+  const scrollRight = true;
+  const wrap = true;
+  gBrowser.tabContainer.addEventListener("wheel", function(event) {
+    let dir = (scrollRight ? 1 : -1) * Math.sign(event.deltaY);
+    setTimeout(function() {
+      gBrowser.tabContainer.advanceSelectedTab(dir, wrap);
+    }, 0);
+  }, true);
+})();
+
+
+
+
+// 05. 在新标签页查看图片 
+location.href.startsWith('chrome://browser/content/browser.x') && (function () {
+    document.querySelector("#context-viewimage").setAttribute("oncommand", 'openTrustedLinkIn(gContextMenu.imageURL,"tab")');
+})();
+
+
+
+
+// 06. 鼠标移动到地址栏和搜索栏时自动全选里面的文字
+/* {if (location == "chrome://browser/content/browser.xul") {
+// 地址栏
+var autselectpulbar = document.getElementById("urlbar-container");
+autselectpulbar.addEventListener("mouseover", function(event){
+            if(event.target.compareDocumentPosition(document.activeElement)!= 20)
+                   event.target.select();
+    }, false);
+
+// 搜索栏
+var autselectpsearchbar = document.getElementById("searchbar");
+ autselectpsearchbar.addEventListener("mouseover", function(event){
+           if(event.target.compareDocumentPosition(document.activeElement)!= 20)
+                   event.target.select();
+}, false);
+}
+};
+*/
+
+//07.  关闭标签页后选择左侧标签   
+(function () {
+  gBrowser.tabContainer.addEventListener("TabClose", tabCloseHandler, false);
+  function tabCloseHandler(event) {
+    var tab = event.target;
+    gBrowser.selectedTab = tab;
+    if (gBrowser.selectedTab._tPos != 0) {
+      gBrowser.tabContainer.advanceSelectedTab(-1, true);
+    }
+  }
+})();
+
+
+
+
+//08.  在当前标签页右侧打开新标签页 
+(function () {
+    if (!location.href.startsWith('chrome://browser/content/browser.x'))
+        return;
+    gBrowser.tabContainer.addEventListener("TabOpen", tabOpenHandler, false);
+    gBrowser.tabContainer.addEventListener("TabClose", tabCloseHandler, false);
+
+		function tabOpenHandler(event) {
+			var tab = event.target;
+			gBrowser.moveTabTo(tab, gBrowser.selectedTab._tPos + 1);
+		}
+    function tabCloseHandler(event) {
+        var tab = event.target;
+      // 如果是因下载而产生的空白页
+      if (tab.linkedBrowser.documentURI.spec == 'about:blank') return;
+      if (tab._tPos <= gBrowser.selectedTab._tPos){
+          if (tab.previousSibling) {
+            gBrowser.selectedTab = tab.previousSibling;
+        	}
+      }
+    }
+})();
+
+
+
+//09.  新标签打开侧边栏历史页面 
+(function() {
+    if (!location.href.startsWith('chrome://browser/content/browser.x'))
+    return;
+
+  eval('PlacesUIUtils.openNodeWithEvent = '  + PlacesUIUtils.openNodeWithEvent.toString()
+    .replace(' && PlacesUtils.nodeIsBookmark(aNode)', '')
+    .replace('getBrowserWindow(window)', 
+      '(window && window.document.documentElement.getAttribute("windowtype") == "navigator:browser") ? window : BrowserWindowTracker.getTopWindow()')
+  );
+
+  let onPopupshowing = function() {
+    let historyMenu = document.getElementById('history-menu');
+    if (!historyMenu._placesView) {
+      new HistoryMenu(event);
+      historyMenu._placesView._onCommand = function HM__onCommand(aEvent) {
+        let placesNode = aEvent.target._placesNode;
+        if (placesNode) {
+          PlacesUIUtils.openNodeWithEvent(placesNode, aEvent);
+        };
+      };
+    };
+  };
+
+  let historyPopup = document.getElementById('goPopup');
+  historyPopup.setAttribute('onpopupshowing', '(' + onPopupshowing.toString() + ')()');
+
+})();
+
+
---
+++ Updates/UpdatesFiles/_keychanger.js
@@ -0,0 +1,176 @@
+//Firefox 自定义快捷 RunningCheese 版 for 64+
+
+//老板键
+//默认为 Ctrl + ~，如需要修改，可在 Firefox\protable.ini 第56行中修改。
+
+
+
+//F1-12键
+//--------------------------------------------------------------------------------------------------------------------------------------------
+keys['F1'] = function() { document.getElementById("cmd_newNavigatorTab").doCommand();}; //新建标签页并光标定位到地址栏
+keys['F2'] = "duplicateTabIn(gBrowser.selectedTab, 'tab')";//复制当前标签页
+keys['F3'] = function(ev) {BrowserPageInfo();}; //查看页面信息
+keys['F4'] ="gBrowser.selectedTab.toggleMuteAudio()"; // 关闭当前标签声音
+//keys['F5'] =""; // 原生按钮：刷新
+//keys['F6'] =""; // 原生按钮：定位到地址栏
+//keys['F7'] =""; // 原生按钮：启用浏览光标
+keys['F8'] =function() { gBrowser.loadURI("javascript:%20void((function()%20{var%20element%20=%20document.createElement('script');element.id%20=%20'outfox_seed_js';element.charset%20=%20'utf-8',element.setAttribute('src',%20'http://fanyi.youdao.com/web2/seed.js?'%20+%20Date.parse(new%20Date()));document.body.appendChild(element);})())", {triggeringPrincipal: gBrowser.contentPrincipal});}; //启用翻译功能
+keys['F9'] = function() {
+                gBrowser.loadURI("javascript:(function(bookmarklets)%7Bfor(var%20i=0;i%3Cbookmarklets.length;i++)%7Bvar%20code=bookmarklets%5Bi%5D.url;if(code.indexOf(%22javascript:%22)!=-1)%7Bcode=code.replace(%22javascript:%22,%22%22);eval(code)%7Delse%7Bcode=code.replace(/%5Es+%7Cs+$/g,%22%22);if(code.length%3E0)%7Bwindow.open(code)%7D%7D%7D%7D)(%5B%7Btitle:%22%E7%A0%B4%E9%99%A4%E5%8F%B3%E9%94%AE%E8%8F%9C%E5%8D%95%E9%99%90%E5%88%B6%22,url:%22javascript:function%20applyWin(a)%7Bif(typeof%20a.__nnANTImm__===%5Cx22undefined%5Cx22)%7Ba.__nnANTImm__=%7B%7D;a.__nnANTImm__.evts=%5B%5Cx22mousedown%5Cx22,%5Cx22mousemove%5Cx22,%5Cx22copy%5Cx22,%5Cx22contextmenu%5Cx22%5D;a.__nnANTImm__.initANTI=function()%7Ba.__nnantiflag__=true;a.__nnANTImm__.evts.forEach(function(c,b,d)%7Ba.addEventListener(c,this.fnANTI,true)%7D,a.__nnANTImm__)%7D;a.__nnANTImm__.clearANTI=function()%7Bdelete%20a.__nnantiflag__;a.__nnANTImm__.evts.forEach(function(c,b,d)%7Ba.removeEventListener(c,this.fnANTI,true)%7D,a.__nnANTImm__);delete%20a.__nnANTImm__%7D;a.__nnANTImm__.fnANTI=function(b)%7Bb.stopPropagation();return%20true%7D;a.addEventListener(%5Cx22unload%5Cx22,function(b)%7Ba.removeEventListener(%5Cx22unload%5Cx22,arguments.callee,false);if(a.__nnantiflag__===true)%7Ba.__nnANTImm__.clearANTI()%7D%7D,false)%7Da.__nnantiflag__===true?a.__nnANTImm__.clearANTI():a.__nnANTImm__.initANTI()%7DapplyWin(top);var%20fs=top.document.querySelectorAll(%5Cx22frame,%20iframe%5Cx22);for(var%20i=0,len=fs.length;i%3Clen;i++)%7Bvar%20win=fs%5Bi%5D.contentWindow;try%7Bwin.document%7Dcatch(ex)%7Bcontinue%7DapplyWin(fs%5Bi%5D.contentWindow)%7D;void%200;%22%7D,%7Btitle:%22%E7%A0%B4%E9%99%A4%E9%80%89%E6%8B%A9%E5%A4%8D%E5%88%B6%E9%99%90%E5%88%B6%22,url:%22javascript:(function()%7Bvar%20doc=document;var%20bd=doc.body;bd.onselectstart=bd.oncopy=bd.onpaste=bd.onkeydown=bd.oncontextmenu=bd.onmousemove=bd.onselectstart=bd.ondragstart=doc.onselectstart=doc.oncopy=doc.onpaste=doc.onkeydown=doc.oncontextmenu=null;doc.onselectstart=doc.oncontextmenu=doc.onmousedown=doc.onkeydown=function%20()%7Breturn%20true;%7D;with(document.wrappedJSObject%7C%7Cdocument)%7Bonmouseup=null;onmousedown=null;oncontextmenu=null;%7Dvar%20arAllElements=document.getElementsByTagName(%5Cx27*%5Cx27);for(var%20i=arAllElements.length-1;i%3E=0;i--)%7Bvar%20elmOne=arAllElements;with(elmOne.wrappedJSObject%7C%7CelmOne)%7Bonmouseup=null;onmousedown=null;%7D%7Dvar%20head=document.getElementsByTagName(%5Cx27head%5Cx27)%5B0%5D;if(head)%7Bvar%20style=document.createElement(%5Cx27style%5Cx27);style.type=%5Cx27text/css%5Cx27;style.innerHTML=%5Cx22html,*%7B-moz-user-select:auto!important;%7D%5Cx22;head.appendChild(style);%7Dvoid(0);%7D)();%22%7D%5D)", {triggeringPrincipal: gBrowser.contentPrincipal});
+                };// 解除右键菜单限制
+keys['F10'] = function(){var newtabs=["https://weibo.com/","https://www.inoreader.com/","https://www.twitter.com/","https://www.runningcheese.com/"];var i=0;while(i<=newtabs.length-1){gBrowser.selectedTab=gBrowser.addTrustedTab(newtabs[i]);i=i+1;}};//一键打开标签组
+
+
+//Alt组合键
+//--------------------------------------------------------------------------------------------------------------------------------------------
+keys["Alt+F1"] = function(){for (let i = gBrowser.selectedTab ._tPos - 1; i >= 0; i--) if (!gBrowser.tabs[i].pinned){ gBrowser.removeTab(gBrowser.tabs[i], {animate: true});}};  //关闭左侧所有标签页
+keys["Alt+F2"] = function(){gBrowser.removeTabsToTheEndFrom(gBrowser.selectedTab );};  //关闭右侧所有标签页
+keys["Alt+F3"] = function(){gBrowser.removeAllTabsBut(gBrowser.selectedTab);};  //关闭其他标签页
+keys['Alt+W'] = 'document.getElementById("pageAction-urlbar-_cd7e22de-2e34-40f0-aeff-cec824cbccac_").click();'//稍后阅读标记/去除标记
+keys['Alt+E'] = 'ReaderParent.toggleReaderMode(event);';//阅读模式
+keys['Alt+Y'] = function () {  UploadImages(); };//上传图片（需要登录新浪微博）
+keys['Alt+G'] = "var s = prompt('谷歌站内搜索:', '');if (s.length > 0) gBrowser.addTrustedTab('https://www.google.com/search?q=site:' + encodeURIComponent( gBrowser.currentURI.host) + ' ' + encodeURIComponent(s));";//Google站内搜索
+keys['Alt+B'] = "var s = prompt('百度站内搜索:', '');if (s.length > 0) gBrowser.addTrustedTab('https://www.baidu.com/s?wd=site:' + encodeURIComponent( gBrowser.currentURI.host) + ' ' + encodeURIComponent(s));";//Baidu站内搜索
+keys['Alt+X'] = "getWebNavigation().canGoForward && getWebNavigation().goForward();";//前进
+keys['Alt+Z'] = "getWebNavigation().canGoBack && getWebNavigation().goBack();";//后退
+keys['Alt+C'] = function() { var gClipboardHelper = Components.classes['@mozilla.org/widget/clipboardhelper;1'].getService(Components.interfaces.nsIClipboardHelper);gClipboardHelper.copyString(gBrowser.currentURI.spec); }; //复制当前页的URL
+keys['Alt+V'] =function() {let url = readFromClipboard();
+try {
+            switchToTabHavingURI(url, true);
+          } catch (ex) {
+            var reg = /[a-zA-Z0-9][-a-zA-Z0-9]{0,62}(\.[a-zA-Z0-9][-a-zA-Z0-9]{0,62})+\.?/;
+            if (!reg.test(url)) {
+              url = 'https://www.baidu.com/s?wd=' + encodeURIComponent(url);
+            } else {
+              if (url.substring(4, 0).toLowerCase() == "http") {
+                url = encodeURIComponent(url);
+              } else {
+                url = 'http://' + encodeURIComponent(url);
+              }
+            }
+            switchToTabHavingURI(url, true);
+          }
+          e.preventDefault();
+          e.stopPropagation();
+}; //打开剪切板地址
+keys['Alt+U'] = function(ev) {BrowserPageInfo();}; //查看页面信息
+keys['Alt+I'] = function() {
+	try {
+		var path ="..\\..\\..\\Program Files\\Internet Explorer\\iexplore.exe";	var file = Components.classes["@mozilla.org/file/local;1"].createInstance(Components.interfaces.nsIFile);file.initWithPath(path.replace(/^\./, Components.classes["@mozilla.org/file/directory_service;1"].getService(Components.interfaces.nsIProperties).get("SysD", Components.interfaces.nsIFile).path));file.launch();
+
+		var process = Cc["@mozilla.org/process/util;1"].createInstance(Ci.nsIProcess);
+		process.init(file);
+		process.run(false, [gBrowser.currentURI.spec], 1);
+	} catch (ex) {
+		alert("打开IE失败!")
+	}
+}; //用IE打开当前页
+keys['Alt+N'] =  function() {
+                gBrowser.loadURI("javascript:(function(){var%20night=function(w){(function(d){var%20css='html{opacity:0.7!important;background:black!important;}body{background:white!important;}';var%20s=d.getElementsByTagName('style');for(var%20i=0,si;si=s[i];i++){if(si.innerHTML==css){si.parentNode.removeChild(si);return}};var%20heads=d.getElementsByTagName('head');if(heads.length){var%20node=d.createElement('style');node.type='text/css';node.appendChild(d.createTextNode(css));heads[0].appendChild(node)}})(w.document);%20for(var%20i=0,f;f=w.frames[i];i++){try{arguments.callee(f)}catch(e){}}};night(window)})();", {triggeringPrincipal: gBrowser.contentPrincipal});
+                };// 夜间模式
+keys['Alt+O'] = "openPreferences();"; //Firefox选项
+keys['Alt+P'] = "OpenBrowserWindow({private: true});"; //打开隐私窗口
+keys['Alt+]'] =  'TabStickOnTop();';  //将当前窗口置顶
+
+
+
+//Shift 组合键
+// keys['Shift+F1'] =""; // 侧边栏列出打开标签
+keys['Shift+F2'] =function() {var listenOpen = {
+	observe: function(aSubject, aTopic, aData) {
+		if (aTopic != 'domwindowopened') return;
+		var newWin = aSubject;
+		newWin.addEventListener('load', function onLoad() {
+			setTimeout(function() {
+				doNewWin(newWin);
+				newWin.removeEventListener('load', onLoad);
+			}, 0);
+		});
+	}
+};
+function doNewWin(newWin) {
+	const dialogText = 'Eine eingehende Anfrage für eine externe Debugger-Verbindung wurde erkannt.'
+	if (newWin.location == 'chrome://global/content/commonDialog.xul' && newWin.document.getElementById('info.body').textContent.startsWith(dialogText)) {
+		var button = newWin.document.getAnonymousElementByAttribute(
+		newWin.document.documentElement, 'dlgtype', 'accept');
+		button.click();
+		Services.ww.unregisterNotification(listenOpen);
+		clearTimeout(tId);
+	};
+};
+
+Services.ww.registerNotification(listenOpen);
+if (!document.getElementById('menu_browserToolbox')) {
+	let {
+		require
+	} = Cu.import("resource://devtools/shared/Loader.jsm", {});
+	require("devtools/client/framework/devtools-browser");
+};
+document.getElementById('menuitem_inspector').click();
+var tId = setTimeout(function() {
+	Services.ww.unregisterNotification(listenOpen);
+}, 5000);}; // 打开工具箱
+keys['Shift+F3'] =function() {var listenOpen = {
+	observe: function(aSubject, aTopic, aData) {
+		if (aTopic != 'domwindowopened') return;
+		var newWin = aSubject;
+		newWin.addEventListener('load', function onLoad() {
+			setTimeout(function() {
+				doNewWin(newWin);
+				newWin.removeEventListener('load', onLoad);
+			}, 0);
+		});
+	}
+};
+function doNewWin(newWin) {
+	const dialogText = 'Eine eingehende Anfrage für eine externe Debugger-Verbindung wurde erkannt.'
+	if (newWin.location == 'chrome://global/content/commonDialog.xul' && newWin.document.getElementById('info.body').textContent.startsWith(dialogText)) {
+		var button = newWin.document.getAnonymousElementByAttribute(
+		newWin.document.documentElement, 'dlgtype', 'accept');
+		button.click();
+		Services.ww.unregisterNotification(listenOpen);
+		clearTimeout(tId);
+	};
+};
+
+Services.ww.registerNotification(listenOpen);
+if (!document.getElementById('menu_browserToolbox')) {
+	let {
+		require
+	} = Cu.import("resource://devtools/shared/Loader.jsm", {});
+	require("devtools/client/framework/devtools-browser");
+};
+document.getElementById('menu_browserToolbox').click();
+var tId = setTimeout(function() {
+	Services.ww.unregisterNotification(listenOpen);
+}, 5000);}; //打开浏览器工具箱
+// keys['Shift+F4'] =""; // 打开 代码草稿纸
+
+//Ctrl+Alt组合键
+//--------------------------------------------------------------------------------------------------------------------------------------------
+
+keys['Ctrl+Alt+A'] =  function() {FileUtils.getFile('UChrm',['Local', 'Snapshot.exe']).launch();};//截图
+keys['Ctrl+Alt+Shift+A'] =  function() {document.getElementById("titlebar-min").click(); FileUtils.getFile('UChrm',['Local', 'Snapshot.exe']).launch();};//隐藏火狐截图
+keys['Ctrl+Alt+Q'] =  function() {FileUtils.getFile('UChrm',['Local', 'FSCapture','FSCapture.exe']).launch();};//完整截图
+keys["Ctrl+Alt+S"]  = function() {FileUtils.getFile('UChrm',['Local', 'ScreenToGif.exe']).launch();};//GIF截图
+keys["Ctrl+Alt+R"]  = function() {FileUtils.getFile('UChrm',['Local', 'Shadowsocks','ShadowsocksR.exe']).launch();};//启动SS程序
+keys["Ctrl+Alt+F"]  = function() {FileUtils.getFile('UChrm',['Local', 'Firemin','Firemin.exe']).launch();};//启动内存管理软件
+keys['Ctrl+Alt+C'] =  function() {FileUtils.getFile('UChrm',['Local', 'Colors','Colors.exe']).launch();};//颜色拾取器
+keys["Ctrl+Alt+X"]  = function() {var toolbar = document.getElementById("toolbar-menubar");var visibility = toolbar.getAttribute("autohide") == "true";setToolbarVisibility(toolbar, visibility);};//打开Alt菜单 ff70+
+
+
+
+
+//Ctrl+Shift组合键
+//--------------------------------------------------------------------------------------------------------------------------------------------
+//keys['Ctrl+Shift+A'] = 原生快捷键：打开附加组件栏
+//keys['Ctrl+Shift+S'] = 原生快捷键：打开火狐自带的截图功能
+//keys['Ctrl+Shift+D'] = 原生快捷键：保存当前所有标签
+keys['Ctrl+Shift+Z'] =function() {openPreferences();};//打开选项
+
+
+
+
+
+
+
