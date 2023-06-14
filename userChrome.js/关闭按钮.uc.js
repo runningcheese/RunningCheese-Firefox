5be1e0396cc25207a4a310f14beddbd03cc0c8a1
RunningChese <406417036@qq.com>
2023-06-14 15:07:20 +0800

    Update 关闭按钮.uc.js

--- userChrome.js/关闭按钮.uc.js
+++ userChrome.js/关闭按钮.uc.js
@@ -1,7 +1,7 @@
     // ==UserScript==
     // @charset        UTF-8
     // @description    定义浏览器关闭按钮,中键为重启浏览器,右键为退出浏览器。并定义浏览器最大化按钮右键为新建窗口
-    // @version        2017.03.08 11
+    // @version        2017.03.08 222222
     // ==/UserScript==
     document.getElementById('titlebar-close').setAttribute("tooltiptext"," 中键：重启  右键：退出浏览器");
     document.getElementById('titlebar-close').onclick=function (event) {
